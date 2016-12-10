0f5d13cb5d0a788c6cb5a29ea36fbbc9b5800bf8
Alex Lapinski <contact@alexlapinski.name>
2016-12-09 23:32:47 -0500

    First use of ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate ndarray;
+extern crate num_traits;
+
+
+use ndarray::Array;
+use ndarray::arr2;
+use ndarray::Ix2;
+
+use num_traits::Float;
+
+fn standardize<A: Float>(data: Array<A, Ix2>) -> Array<A, Ix2> {
+    return data;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Linear Regression Test Bench");
+    let arr = arr2(&[[1., 2.], [3., 4.]]);
+
+    println!("\n");
+    println!("Data: {}", arr);
+
+    let std_arr = standardize(arr);
+    println!("Standardized Data: {}", std_arr)
 }
